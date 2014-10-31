479fb90c4f291ef31ab683484d448b97963371ea
RASmith1 <bucknbeck@verizon.net>
2014-10-30 21:43:46 -0400

    Modified app.php and composer.json

--- app/config/app.php
+++ app/config/app.php
@@ -191,7 +191,7 @@ return array(
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
 		'Pre'             => 'Paste\Pre',
-		'cURL'            => 'anlutro\cURL\Laravel\cURL' 
+		'cURL'            => 'anlutro\cURL\Laravel\cURL', 
 
 	),
 
