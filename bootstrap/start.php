145cedd55a61e4129ccdcb33d4727e0f53273f12
RASmith1 <bucknbeck@verizon.net>
2014-10-31 10:56:38 -0400

    modified start.php

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -27,7 +27,8 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('*'),
+	'local' => array('Escalade-PC'),
+        'production' => array('RebeccaSmith'),
 
 ));
 
